98a33ac3d918d74978c430c154fe7b95c4d9794d
TouchAssemblyServices <tmediassembly@gmail.com>
2020-05-09 01:11:48 +0000

    Intro to Partials

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,7 @@ class Story < ApplicationRecord
     validates  :name,  :link,
 presence:  true
     has_many  :votes
+    def to_param
+        "#{id}-#{name.gsub(/\W/, '_').downcase}"
+    end    
 end
